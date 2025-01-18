5d170149e961cddf3650e7f0e8d205bc6a98e195
Rithvik Harinath <rithvikharinath@gmail.com>
2025-01-17 17:17:40 -0800

    IE2 Step 5

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,14 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -55,6 +63,22 @@ app.get("/users", (req, res) => {
   } else {
     res.send(users);
   }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.listen(port, () => {
